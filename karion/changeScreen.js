42964d773f18963ce052373c07584114e1dfcd9b
Pouya Kary <k@arendelle.org>
2015-01-12 00:26:22 +0330

    a karion test

---
+++ karion/changeScreen.js
@@ -0,0 +1,14 @@
+
+var box = document.getElementById("box");
+var karion = document.getElementById("karion");
+var boxWidth = box.style.width;
+
+if ( boxWidth < 800 ) {
+
+	karion.style.fontSize = "12vw";
+
+} else {
+
+	karion.style.fontSize = "4cm;"
+
+}
